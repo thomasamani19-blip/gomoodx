4ad98f47ae1b7e096bdcba656d5e554bcd6fcada
Thomas Amani <amanignangoran104@gmail.com>
2025-10-15 12:44:00 +0000

    Super je valide

--- src/app/(app)/admin/abonnements/page.tsx
+++ src/app/(app)/admin/abonnements/page.tsx
@@ -1,10 +1,11 @@
+
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAuth } from '@/hooks/use-auth';
