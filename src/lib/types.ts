37373fb0b71cfaab28119193484a5e0c4eb4732e
Thomas Amani <amanignangoran104@gmail.com>
2025-10-16 18:50:50 +0000

    Oui

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -100,7 +100,6 @@ export interface EstablishmentPricing {
 
 export interface CreatorRates {
     videoCallPerMinute?: number;
-    voiceCallPerMinute?: number;
     escortPerHour?: number;
     escortHalfDay?: number; // For 4 hours
     escortFullDay?: number; // For 8 hours
