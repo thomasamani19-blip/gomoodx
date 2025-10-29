871ded9a9007cb7ff4f05d2b096509eed582ddf9
Thomas Amani <amanignangoran104@gmail.com>
2025-10-29 14:07:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/page.tsx
+++
@@ -1,6 +0,0 @@
-// This file is intentionally left blank.
-// The root of the (app) group does not need a page.
-// The middleware will handle redirection to a default page like /dashboard.
-export default function AppRootPage() {
-  return null;
-}
